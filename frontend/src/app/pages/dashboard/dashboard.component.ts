ee823c9fbbe5cb5e37ca57245f3ce9c82720ad9d
PedroArthur1 <pedroarthur0001@gmail.com>
2026-08-11 22:16:03 -0300

    feat(frontend): refatora layout do dashboard para utilizar sidebar e header e altera css global

--- frontend/src/app/pages/dashboard/dashboard.component.ts
+++ frontend/src/app/pages/dashboard/dashboard.component.ts
@@ -1,26 +1,14 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { Router } from '@angular/router';
-import { AuthService } from '../../services/auth/auth.service';
+import { Sidebar } from '../../components/sidebar/sidebar';
+import { Header } from '../../components/header/header';
+import { Welcome } from './welcome/welcome';
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, Sidebar, Header, Welcome],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.css',
 })
-export class DashboardComponent {
-  role: string | null;
-
-  constructor(private authService: AuthService, private router: Router) {
-    this.role = this.authService.getRole();
-  }
-
-  onLogout(): void {
-    this.authService.logout().subscribe({
-      next: () => this.router.navigate(['/login']),
-      error: () => this.router.navigate(['/login'])
-    });
-  }
-}
+export class DashboardComponent {}
